8019c15234789fc9b555387c8ab48cc7baa740a8
anson1014 <anson1014@hotmail.com>
2020-04-16 19:18:37 -0700

    polished UI

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -4,23 +4,31 @@ const weatherForm = document.querySelector('form');
 const search = document.querySelector('input');
 const curLocation = document.getElementById('location');
 const forecast = document.getElementById('forecast');
+const loader = document.getElementById('preloader');
+const weather = document.getElementById('weather-wrapper');
 
 weatherForm.addEventListener('submit', (e) => {
     e.preventDefault();
 
+    loader.style.display = 'block';
+    curLocation.innerHTML = '';
+    forecast.innerHTML = '';
+    weather.style.display = 'none';
     const location = search.value;
 
-    fetch(`/weather?address=${location}`).then(
-        (response) => {
-            response.json().then((data) => {
-                if (data.error) {
-                    curLocation.innerHTML = data.error;
-                    forecast.innerHTML = '';
-                } else {
-                    curLocation.innerHTML = data.location;
-                    forecast.innerHTML = data.forecast;
-                }
-            });
-        }
-    );
+    fetch(`/weather?address=${location}`).then((response) => {
+        response.json().then((data) => {
+            if (data.error) {
+                curLocation.innerHTML = data.error;
+                forecast.innerHTML = '';
+                loader.style.display = 'none';
+            } else {
+                curLocation.innerHTML = data.location;
+                forecast.innerHTML = data.forecast + 'º';
+                loader.style.display = 'none';
+                weather.style.display = 'flex';
+                console.log(weather);
+            }
+        });
+    });
 });
--- web-server/src/utils/forecast.js
+++ web-server/src/utils/forecast.js
@@ -8,10 +8,7 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location', undefined);
         } else {
-            callback(
-                undefined,
-                `It is ${body.current.temperature} degrees. It feels like ${body.current.feelslike} degrees. There is a ${body.current.precip}% chance of rain.`
-            );
+            callback(undefined, `${body.current.temperature}`);
         }
     });
 };
